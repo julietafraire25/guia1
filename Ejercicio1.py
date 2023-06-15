ff9245a7dc8ad5d0121ad6a70a60c3cc186976a1
julietafraire25 <julietafraire@gmail.com>
2023-06-15 18:03:42 -0300

    primer commit

---
+++ Ejercicio1.py
@@ -0,0 +1,11 @@
+#Generando el input
+velocidad = float(input("Ingrese la velocidad: "))
+tiempo = float (input("Ingrese el tiempo: "))
+
+#Proceso
+distancia = velocidad * tiempo
+
+#Generando el output
+print("La distancia recorrida es: ", distancia)
+
+#segundo comentario
